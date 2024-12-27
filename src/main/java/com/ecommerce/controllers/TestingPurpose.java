441fd16fad5feea3d8024823fccb1c8b1d2e5f85
Tarun Tadepalli <taruntadepalli750@gmail.com>
2024-12-27 16:27:20 -0500

    Version 1 Restore

--- src/main/java/com/ecommerce/controllers/TestingPurpose.java
+++ src/main/java/com/ecommerce/controllers/TestingPurpose.java
@@ -5,7 +5,6 @@ public class TestingPurpose {
 	public static void main(String[] args) {
 		
 		System.out.println("This is a test File for git.");
-		System.out.println("This is change for Version 3.");
 	}
 
 }
